d6bd3f0a76ff0c1a2c3a5f66f2b69c1c83fb939a
Ashf5 <friedash85@gmail.com>
2025-07-24 10:03:49 +0300

    add completed buttons

--- frontend/src/components/CompleteTask.js
+++ frontend/src/components/CompleteTask.js
@@ -19,10 +19,32 @@ function CompleteTask(props) {
 
     }, [])
 
+    function completedHandler(e) {
+        console.log(bookings)
+        let id = bookings[e.target.name].id;
+        const updateStatus = async () => {
+            let response = await fetch(`http://localhost:5000/api/bookings/status/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    status: 'completed'
+                })
+            })
+            if (response.status === 201) {
+                let copy = bookings.map(booking => ({...booking}));
+                copy[e.target.name].status = 'completed'
+                setBookings(copy);
+            }
+        }
+        updateStatus()
+    }
+
     return (
         <div>
             <ul>
-                {bookings.map((booking, i) => <li key={i}>{booking.name} {booking.email} {booking.phone} {booking.address} {booking.date} {booking.start_time} {booking.status} <button>Mark Completed</button></li>)}
+                {bookings.map((booking, i) => <li key={i}>{booking.name} {booking.email} {booking.phone} {booking.address} {booking.date} {booking.start_time} {booking.status} <button onClick={completedHandler} name={i}>Mark Completed</button></li>)}
             </ul>
             
         </div>
